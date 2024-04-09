8ffad0f93ba8387769ad38dafff7235310c65408
LndTrinity <leonardoperezdaluz@gmail.com>
2024-04-08 23:57:18 -0300

    Adicionando função soma

--- calculator.js
+++ calculator.js
@@ -1,16 +1,102 @@
+const prompt = require("prompt-sync")();
+
 // retorna o valor da soma entre os dois parâmetros
-function sum(v1, v2) {}
+function sum(v1, v2) {
+    soma = v1 + v2   
+    
+
+    return(soma)
+}
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
+function sub(v1, v2) {
+   
+
+   return(subtracao)
+
+ }
 
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
+function div(v1, v2) {
+    
+
+    return(divisao)
+ }
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) {
+    
+
+    return(multiplicacao)
+ }
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) { 
+   
 
-console.log(sum(v1, v2));
+    return(raiz)
+}
+
+//console.log(sum(v1, v2));
+while (true) {
+    console.log("=======Calculadora========")
+    console.log("Digite a operação desejada")
+    console.log("1. Soma")
+    console.log("2. Subtração")
+    console.log("3. Divisão")
+    console.log("4. Multiplicação")
+    console.log("5. Raiz Quadrada")
+    console.log("6. Sair")
+    const operacao = Number(prompt("Digite a operação desejada: "))
+   
+
+    if (operacao == 1) {
+        const v1 = Number(prompt("Digite o primeiro Número: "))
+        const v2 = Number(prompt("Digite o segundo Número: "))
+        sum(v1, v2)
+        console.log()
+        console.log("Resultado: " + v1 + " + " + v2 + " = " + soma)
+        console.log()
+    } else if (operacao == 2) {
+        const v1 = Number(prompt("Digite o primeiro Número: "))
+        const v2 = Number(prompt("Digite o segundo Número: "))
+        sub(v1, v2)
+        console.log()
+        console.log("Resultado: " + v1 + " - " + v2 + " = " + subtracao)
+        console.log()
+    } else if (operacao == 3) {
+        const v1 = Number(prompt("Digite o primeiro Número: "))
+        const v2 = Number(prompt("Digite o segundo Número: "))
+        div(v1, v2)
+        console.log()
+        console.log("Resultado: " + v1 + " / " + v2 + " = " + divisao)
+        console.log()
+    } else if (operacao == 4) {
+        const v1 = Number(prompt("Digite o primeiro Número: "))
+        const v2 = Number(prompt("Digite o segundo Número: "))
+        mult(v1, v2)
+        console.log()
+        console.log("Resultado: " + v1 + " * " + v2 + " = " + multiplicacao)
+        console.log()
+    } else if (operacao == 5) {
+        const v1 = Number(prompt("Digite o Número: "))
+        
+        square(v1)
+        console.log()
+        console.log("Resultado da raiz quadrada de " + v1 +  " : " + raiz)
+        console.log() 
+    }
+    else if (operacao == 6) {
+        console.log()
+        console.log("Good Bye")
+        console.log()
+        break
+    } 
+    else {
+        console.log()
+        console.log("Insira uma opção válida!")
+        console.log()
+    }
+}
+
+
