52cd64399e4286c2c7f7abdad0cc37a782fcee3d
Israel Jeus <wlisrael6@gmail.com>
2024-04-10 19:34:26 -0300

    Adicionando as operações matemáticas

--- calculator.js
+++ calculator.js
@@ -9,7 +9,7 @@ function sum(v1, v2) {
 
 // retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {
-   
+    subtracao = v1 -v2
 
    return(subtracao)
 
@@ -17,21 +17,21 @@ function sub(v1, v2) {
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {
-    
+    divisao = v1 / v2
 
     return(divisao)
  }
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {
-    
+    multiplicacao = v1 * v2
 
     return(multiplicacao)
  }
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) { 
-   
+    raiz =  Math.sqrt(v1)
 
     return(raiz)
 }
